22663ff2a937dd9dda174ce7f8a2c1ff84233447
Crauzer <filip.quitko@gmail.com>
2020-06-19 18:00:10 +0200

    Update bin.rs

--- src/io/bin.rs
+++ src/io/bin.rs
@@ -399,9 +399,9 @@ impl BinValue {
     fn pack_value_type(value_type: BinValueType) -> u8 {
         let mut value_type = value_type.to_u8().expect("Invalid Value Type");
 
-        if value_type >= 18 && value_type <= 24
+        if value_type >= 18 && value_type <= 25
         {
-            value_type = (value_type - 18) | 128;
+            value_type = (value_type - 18) + 128;
         }
 
         value_type
